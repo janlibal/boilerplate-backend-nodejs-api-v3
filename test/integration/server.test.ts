57f8c291c2bbe47d4f99d5894210cb76b9b105d4
Jan Libal <libalj@seznam.cz>
2024-02-01 14:12:16 +0100

    Server test implementation

---
+++ test/integration/server.test.ts
@@ -0,0 +1,35 @@
+import supertest from 'supertest'
+import os from 'os'
+import app from '../../src/utils/app'
+import pkg from '../../package.json'
+
+const server = app.listen()
+
+afterAll(async () => {
+    await server.close()
+})
+
+
+describe('GET /', () => {
+      it('<200> should always return with the API server information', async () => {
+        const request = supertest(server)
+        const res = await request
+          .get('/')
+          .expect('Content-Type', /json/)
+          .expect(200)
+  
+        const info = res.body;
+        const expected = ['status', 'message']
+        expect(Object.keys(info)).toEqual(expect.arrayContaining(expected))
+        expect(info.status).toBe('success')
+        expect(info.message.name).toBe(pkg.name)
+        expect(info.message.version).toBe(pkg.version)
+        expect(info.message.description).toBe(pkg.description)
+        expect(info.message.environments).toBeInstanceOf(Object)
+  
+        const environments = info.message.environments
+        expect(environments.hostname).toBe(os.hostname())
+        expect(environments.nodeVersion).toBe(process.versions['node'])
+        expect(environments.platform).toBe(`${process.platform}/${process.arch}`)
+      })
+   })
