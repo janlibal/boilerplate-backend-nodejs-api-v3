e8e2c2833a4ebd834d39f730099b8b62254cad8c
Jan Libal <libalj@seznam.cz>
2024-02-03 09:37:15 +0100

    Wiring Postgres to server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import config from './config'
+import { close, connect } from './database'
 import logger from './utils/logger'
 import createServer from './utils/server'
 
 export async function server(){
 
   let server: any
+  let db: any
   const srvPort = config.server.port
-
+  const dbPort = config.database.connection
 
   try {
+
+    db = await connect()
+    logger.info(`Database connected on ${dbPort}. `)
 
     server = createServer
     logger.info(`Server is listening on ${srvPort}. `)
@@ -25,8 +30,15 @@ export async function server(){
       logger.debug("Server closed")
     }
 
+    if (!db){
+      logger.debug("Closing database...")
+      await close()
+      logger.debug("Database closed")
+      }
+
   }
 
 }
+
 
 server()
