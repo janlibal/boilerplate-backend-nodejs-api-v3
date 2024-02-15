289ec2b86b6ae26d1a4b657c45cdc6e69d993852
Jan Libal <libalj@seznam.cz>
2024-02-15 14:14:28 +0100

    findByEmail() implementation

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,11 +1,11 @@
 import  { User }  from "../database/models"
 import { IUser } from "../interfaces/IUser"
 
-
-
 async function findByEmail(email: string) {
-    return false
-} 
+    let user: any
+    user = await User.query().where('email', email).first()
+    return user
+}
 
 async function saveUser(attributes:IUser) {
     const user = {
