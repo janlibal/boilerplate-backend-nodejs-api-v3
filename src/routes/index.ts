4669e4254579049ce4bf2b04dfe0f3a2a879642a
Jan Libal <libalj@seznam.cz>
2024-02-15 09:26:28 +0100

    /api/v1/user route implementation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,16 @@
 import Router from 'koa-router'
 const router = new Router()
+const api = new Router()
 const test = new Router()
 
 import testRoutes from './testRoutes'
+import userRoutes from './userRoutes'
 
 test.use(testRoutes)
+api.use(userRoutes)
 
 router.use('', test.routes())
+router.use('/api/v1', api.routes())
 
   
 
