ae045b4a49e93772c28a0e1a90e4d77a22603e7d
Jan Libal <libalj@seznam.cz>
2024-02-15 17:57:11 +0100

    /api/v1/login route implementation

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import Router from "koa-router"
-import { signUp  } from "../controllers/userController"
+import { signUp, signIn } from "../controllers/userController"
 
 const router = new Router()
 
 router.post('/user', signUp)
+router.post('/login', signIn)
 
 export default router.routes()
