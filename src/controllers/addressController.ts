3a1813b7e54ab9be23f4390ffb473132c8ba77af
Jan Libal <libalj@seznam.cz>
2024-03-07 14:23:44 +0100

    UserId test added

--- src/controllers/addressController.ts
+++ src/controllers/addressController.ts
@@ -2,6 +2,7 @@ import { IContext } from "../interfaces/IContext"
 
 export async function address(ctx: IContext){
 
+
     ctx.status = 200
     
 }
--- test/integration/address.test.ts
+++ test/integration/address.test.ts
@@ -10,14 +10,16 @@ afterAll(async () => {
 
 describe('POST /api/v1/address', () => {
 
-    it('1. ADDRESS: Should return 200', async () => {
+    it.only('1. ADDRESS: Should return 200', async () => {
       const request = supertest(server)
       const res = await request
       .post(`/api/v1/address`)
       .expect(200)
 
+      const info = res.body
       const status = res.status
       expect(status).toBe(200)
+      expect(info.userId).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i)
       
   
 
