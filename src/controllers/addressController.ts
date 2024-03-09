c33f8b8cd997342787cbe9d32f8a096782e142b6
Jan Libal <libalj@seznam.cz>
2024-03-09 17:25:54 +0100

    address tests added

--- src/controllers/addressController.ts
+++ src/controllers/addressController.ts
@@ -1,9 +1,17 @@
+import validate from '../validations'
 import { IContext } from "../interfaces/IContext"
 
 export async function address(ctx: IContext){
 
     const userId = ctx.state.userId
 
+    const body = {
+        firstName: ctx.request.body.firstName,
+        lastName: ctx.request.body.lastName,
+        phoneNo: ctx.request.body.phoneNo,
+        address: ctx.request.body.address,
+    }
+    
     const data = {
         userId: userId
     }
