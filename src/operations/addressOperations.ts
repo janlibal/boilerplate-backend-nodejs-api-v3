d5561211c976da330f7534c5b1fd73ecaef8f786
Jan Libal <libalj@seznam.cz>
2024-03-10 19:07:20 +0100

    create() added

---
+++ src/operations/addressOperations.ts
@@ -0,0 +1,34 @@
+import { IContact, IContactData, IUserData } from "../interfaces/IContact"
+import addressRepository from "../repositories/addressRepository"
+import logger from "../utils/logger"
+
+async function create(input: IContact) {
+    
+    logger.info('create address started')
+
+    const fullName = input.lastName + ', ' + input.firstName
+     
+
+    const contactData: IContactData = {
+        firstName: input.firstName,
+        lastName: input.lastName,
+        phoneNo: input.phoneNo,
+        address: input.address,
+    }
+
+    const userData: IUserData = {
+        fullName: fullName,
+        userId: input.userId.toString(),
+    }
+
+    const contact = await addressRepository.save(contactData, userData)
+
+    logger.info('create address finished')
+    
+    return contact
+
+}
+
+export default {
+     create
+}
